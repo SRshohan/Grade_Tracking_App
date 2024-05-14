ca3541adeee4c98bbb30294a6a11667474720dfa
SRshohan <srshohan02@gmail.com>
2024-05-13 20:00:05 -0400

    Started

---
+++ backend/models/grade.py
@@ -0,0 +1,10 @@
+class grade:
+    def __init__(self, user_id, course, components) -> None:
+        self.user_id = user_id
+        self.course = course
+        self.components = components
+
+        """I want to calculate over grade, how many quizzes are there
+        components = {quizzes: [5, 25, 2]}"""
+        def calculate_grade(self):
+            
